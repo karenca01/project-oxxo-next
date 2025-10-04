7eeadfe21130380d49a86c3bc0c9c48e0d91be34
karenca01 <kcervantes01@alumnos.uaq.mx>
2025-10-04 12:17:21 -0600

    Sidebar

---
+++ app/dashboard/_components/_sidebar/NavItem.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+interface NavItemProps{
+    icon: ReactNode
+    path: string
+}
+
+export const NavItem = ({icon, path}: NavItemProps) => {
+    return (
+        <Link href={path}>
+        {icon}
+        </Link>
+    )
+}
+
+export default NavItem;
--- app/dashboard/_components/_sidebar/Sidebar.tsx
+++ app/dashboard/_components/_sidebar/Sidebar.tsx
@@ -1,9 +1,10 @@
 import { LuStore, LuTruck, LuUser, LuUsers, LuWheat } from "react-icons/lu";
+import NavItem from "./NavItem";
 
 export default function Sidebar(){
     return(
         <nav className="w-[10vw] h-[90vh] bg-orange-200 flex flex-col items-center py-20">
-            <LuStore className="text-4xl"/>
+            <NavItem icon={<LuStore className="text-4xl"/>} path="/dashboard"/>
             <LuTruck className="text-4xl"/>
             <LuWheat className="text-4xl"/>
             <LuUser className="text-4xl"/>
