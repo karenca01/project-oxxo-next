0a485f674e8306664984835fcc802d25bce9cb62
karenca01 <kcervantes01@alumnos.uaq.mx>
2025-11-15 14:20:55 -0600

    Fix update

--- actions/employees/update.ts
+++ actions/employees/update.ts
@@ -5,18 +5,23 @@ import { authHeaders } from "@/helpers/authHeaders";
 import { revalidateTag } from "next/cache";
 
 export default async function updateEmployee(employeeId: string, formData: FormData) {
-    formData.delete("$ACTION_REF_0")
-    formData.delete("$ACTION_0:1")
-    formData.delete("$ACTION_0:0")
+    const cleanData = new FormData();
+    
+    Array.from(formData.entries()).forEach(([key, value]) => {
+        if (!key.startsWith("$")) {
+            cleanData.append(key, value);
+        }
+    });
 
     const response = await fetch(`${API_URL}/employees/${employeeId}`,{
         method: "PATCH",
         headers: {
             ...authHeaders()            
         },
-        body: formData
+        body: cleanData
     })
     if(response.status === 200) revalidateTag("dashboard:employees");
+    if(response.status === 200) revalidateTag(`dashboard:employees:${employeeId}`);
     // console.log("location =>", formData.get("location"));
     return;
 }
